eaf82e5e2d6698c43d8cf4f91bb741b90ad698e7
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-12-21 05:44:54 +0700

    perf: use react-hook-form in SigninForm

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -1,61 +1,60 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import {
   Button, Container, Stack, TextField, Typography,
 } from '@mui/material';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface SigninFormInput {
+  email: string
+  password: string
+}
 
 export default function SigninForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const {
+    register, handleSubmit, watch, reset,
+  } = useForm<SigninFormInput>();
   const [error, setError] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const onSubmit: SubmitHandler<SigninFormInput> = async (data) => {
     const result = await signIn('credentials', {
-      email,
-      password,
+      email: data.email,
+      password: data.password,
       redirect: false,
     });
 
     if ((result?.error) !== null) {
       setError(true);
-      setEmail('');
-      setPassword('');
+      reset();
     }
   };
   return (
     <Container maxWidth="sm">
       <Stack>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Stack spacing={2}>
             <Typography variant="h5" align="center">กรุณาเข้าสู่ระบบเพื่อเข้าถึงหน้านี้</Typography>
             <TextField
-              id="email"
               label="อีเมล"
               variant="outlined"
               type="email"
-              name="email"
-              value={email}
-              onChange={(e) => { setEmail(e.target.value); }}
               required
+              InputLabelProps={{ shrink: Boolean(watch('email')) }}
+              {...register('email')}
             />
             <TextField
-              id="password"
               label="รหัสผ่าน"
               variant="outlined"
               type="password"
-              name="pass"
-              value={password}
-              onChange={(e) => { setPassword(e.target.value); }}
               required
+              InputLabelProps={{ shrink: Boolean(watch('password')) }}
+              {...register('password')}
             />
             <Button variant="contained" type="submit">
               เข้าสู่ระบบ
             </Button>
-            {error && <Typography color="red" align="center">ไม่สามารถเข้าสู่ระบบได้</Typography>}
+            {error && <Typography color="red" align="center">อีเมลหรือรหัสผ่านไม่ถูกต้อง</Typography>}
           </Stack>
         </form>
       </Stack>
