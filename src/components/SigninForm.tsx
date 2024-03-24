b83543dc40e8a15db6c8b63fc79b939c562d08b8
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-03-24 12:36:10 +0700

    feat: add background when logging in

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -3,7 +3,8 @@
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import {
-  Button, Container, Stack, TextField, Typography,
+  Backdrop,
+  Button, CircularProgress, Container, Stack, TextField, Typography,
 } from '@mui/material';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
@@ -13,51 +14,62 @@ interface SigninFormInput {
 }
 
 export default function SigninForm() {
+  const [open, setOpen] = useState(false);
+
   const {
     register, handleSubmit, watch, reset,
   } = useForm<SigninFormInput>();
   const [error, setError] = useState(false);
   const onSubmit: SubmitHandler<SigninFormInput> = async (data) => {
+    setOpen(true);
     const result = await signIn('credentials', {
       email: data.email,
       password: data.password,
       redirect: false,
     });
-
+    setOpen(false);
     if ((result?.error) !== null) {
       setError(true);
       reset();
     }
   };
   return (
-    <Container maxWidth="sm">
-      <Stack>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Stack spacing={2}>
-            <Typography variant="h5" align="center">กรุณาเข้าสู่ระบบเพื่อเข้าถึงหน้านี้</Typography>
-            <TextField
-              label="อีเมล"
-              variant="outlined"
-              type="email"
-              required
-              InputLabelProps={{ shrink: Boolean(watch('email')) }}
-              {...register('email')}
-            />
-            <TextField
-              label="รหัสผ่าน"
-              variant="outlined"
-              type="password"
-              required
-              InputLabelProps={{ shrink: Boolean(watch('password')) }}
-              {...register('password')}
-            />
-            <Button variant="contained" type="submit">
-              เข้าสู่ระบบ
-            </Button>
-            {error && <Typography color="red" align="center">อีเมลหรือรหัสผ่านไม่ถูกต้อง</Typography>}
-          </Stack>
-        </form>
-      </Stack>
-    </Container>
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Container maxWidth="sm">
+        <Stack>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2}>
+              <Typography variant="h5" align="center">กรุณาเข้าสู่ระบบเพื่อเข้าถึงหน้านี้</Typography>
+              <TextField
+                label="อีเมล"
+                variant="outlined"
+                type="email"
+                required
+                InputLabelProps={{ shrink: Boolean(watch('email')) }}
+                {...register('email')}
+              />
+              <TextField
+                label="รหัสผ่าน"
+                variant="outlined"
+                type="password"
+                required
+                InputLabelProps={{ shrink: Boolean(watch('password')) }}
+                {...register('password')}
+              />
+              <Button variant="contained" type="submit">
+                เข้าสู่ระบบ
+              </Button>
+              {error && <Typography color="red" align="center">อีเมลหรือรหัสผ่านไม่ถูกต้อง</Typography>}
+            </Stack>
+          </form>
+        </Stack>
+      </Container>
+    </>
   );
 }
