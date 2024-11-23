d4fd67af4cecb1881a16867e1507bb699c7098ed
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-11-23 14:41:57 +0700

    fix: add CssBaseline back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import NewAppBar from '@/src/components/NewAppBar';
 import { Metadata } from 'next';
 import LayoutProvider from '@/src/components/LayoutProvider';
+import { CssBaseline } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import styles from './layout.module.css';
 import ToastProvider from '../components/ToastProvider';
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body>
         <LayoutProvider>
           <AppRouterCacheProvider>
+            <CssBaseline />
             <main>
               <NewAppBar />
               <ToastProvider>
